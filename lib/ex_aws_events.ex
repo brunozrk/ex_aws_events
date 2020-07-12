e4c64b23dfd2d59ee9a071d8e7d578b6c1758384
Bruno Zeraik <brunozrk@gmail.com>
2020-07-12 18:22:00 -0300

    initial commit

---
+++ lib/ex_aws_events.ex
@@ -0,0 +1,18 @@
+defmodule ExAwsEvents do
+  @moduledoc """
+  Documentation for `ExAwsEvents`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAwsEvents.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aws_events_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAwsEventsTest do
+  use ExUnit.Case
+  doctest ExAwsEvents
+
+  test "greets the world" do
+    assert ExAwsEvents.hello() == :world
+  end
+end
